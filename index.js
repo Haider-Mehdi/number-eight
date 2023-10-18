a34ec4e76ce3f377c5f76caed83c585b2fe8a987
Haider Mehdi <haidershah659@gmail.com>
2023-10-18 18:29:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("-".repeat(100));
+const additionResult = 5 + 3;
+console.log("Addition Result:", additionResult);
+console.log("-".repeat(100));
+// Subtraction operation
+const subtractionResult = 10 - 2;
+console.log("Subtraction Result:", subtractionResult);
+console.log("-".repeat(100));
+// Multiplication operation
+const multiplicationResult = 4 * 2;
+console.log("Multiplication Result:", multiplicationResult);
+console.log("-".repeat(100));
+// Division operation
+const divisionResult = 16 / 2;
+console.log("Division Result:", divisionResult);
---
+++ index.ts
@@ -0,0 +1,16 @@
+
+console.log("-".repeat(100))
+const additionResult: number = 5 + 3;
+console.log("Addition Result:", additionResult); 
+console.log("-".repeat(100))
+// Subtraction operation
+const subtractionResult: number = 10 - 2;
+console.log("Subtraction Result:", subtractionResult); 
+console.log("-".repeat(100))
+// Multiplication operation
+const multiplicationResult: number = 4 * 2;
+console.log("Multiplication Result:", multiplicationResult); 
+console.log("-".repeat(100))
+// Division operation
+const divisionResult: number = 16 / 2;
+console.log("Division Result:", divisionResult); 
